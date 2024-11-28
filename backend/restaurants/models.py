e11d11e2287f41e75da697069e16fc167bcffdd8
Nanaki <nkania.jpn@gmail.com>
2024-11-28 23:30:25 +0900

    create restaurants models

--- backend/restaurants/models.py
+++ backend/restaurants/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
