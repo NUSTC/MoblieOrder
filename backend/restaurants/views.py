3bb4d31596132aa627d7b48f5c7fe1886e1f46c5
Nanaki <nkania.jpn@gmail.com>
2024-11-28 23:36:33 +0900

    update views.py

--- backend/restaurants/views.py
+++ backend/restaurants/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Restaurant
+from .serializers import RestaurantSerializer
 
 # Create your views here.
+class RestaurantListView(APIView):
+    def get(self, request):
+        restaurants = Restaurant.objects.all()  # 全レストラン取得
+        serializer = RestaurantSerializer(restaurants, many=True)  # シリアライズ
+        return Response(serializer.data)  # JSONレスポンス
