b2d04b7b0f32c3a459a6b1a633b4f86582b371b4
Nanaki <nkania.jpn@gmail.com>
2024-11-29 01:02:48 +0900

    Add API View

--- backend/menus/views.py
+++ backend/menus/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Menu
+from .serializers import MenuSerializer
 
 # Create your views here.
+class MenuListView(APIView):
+    def get(self, request):
+        menus = Menu.objects.all()
+        serializer = MenuSerializer(menus, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = MenuSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
