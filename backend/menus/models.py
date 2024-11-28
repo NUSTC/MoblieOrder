b798c6f82331f7e0a9f98b57d65b992340abeff8
Nanaki <nkania.jpn@gmail.com>
2024-11-29 01:00:42 +0900

    create menu models

--- backend/menus/models.py
+++ backend/menus/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from restaurants.models import Restaurant
 
 # Create your models here.
+class Menu(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name="menus")
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
