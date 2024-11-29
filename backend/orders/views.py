de4b241c4fb4980481249d364035c5c0009b35be
Nanaki <nkania.jpn@gmail.com>
2024-11-29 15:45:20 +0900

    update views.py

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Order
+from .serializers import OrderSerializer
 
 # Create your views here.
+class OrderHistoryView(APIView):
+    def get(self, request):
+        orders = Order.objects.filter(user=request.user).order_by('-created_at')
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
