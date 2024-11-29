2b5482d1c94553e05b4167f3231c4100553d0a7a
Nanaki <nkania.jpn@gmail.com>
2024-11-29 15:32:08 +0900

    update accounts.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserRegistrationSerializer
 
-# Create your views here.
+# 登録用
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User created successfully!'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Login/Logout用
+from django.contrib.auth import authenticate, login, logout
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        # ユーザー認証
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)  # セッションを開始
+            return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+class LogoutView(APIView):
+    def post(self, request):
+        logout(request)  # セッションを終了
+        return Response({"message": "Logged out successfully"}, status=status.HTTP_200_OK)
