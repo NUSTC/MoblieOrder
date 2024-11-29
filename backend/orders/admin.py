67dfc72aaa17ac717cf5b05b83d73ca02a189768
Nanaki <nkania.jpn@gmail.com>
2024-11-29 15:44:33 +0900

    update admin.py

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
 # Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created_at', 'total_price')
+    list_filter = ('created_at',)
+    search_fields = ('user__username',)
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'menu', 'quantity')
+    list_filter = ('order',)
+    search_fields = ('menu__name',)
