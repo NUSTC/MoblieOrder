9497d3a2f15f58e8c64b46f05ff07a8655bfa3e1
Nanaki <nkania.jpn@gmail.com>
2024-12-02 12:45:27 +0900

    update urls.txt

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,22 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="モバイルオーダー API",
+        default_version='v1',
+        description="学食モバイルオーダーシステムのAPIドキュメント",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +41,8 @@ urlpatterns = [
     path('', include('tickets.urls')),
     path('', include('accounts.urls')),
     path('', include('orders.urls')),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
